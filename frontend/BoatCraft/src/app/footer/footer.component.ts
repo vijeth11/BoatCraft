2ee2b9205a32fba17c9c95c902a356f81a91b852
vijeth11 <vijeth11@yahoo.co.in>
2021-02-25 11:48:29 -0500

    completed footer

--- frontend/BoatCraft/src/app/footer/footer.component.ts
+++ frontend/BoatCraft/src/app/footer/footer.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  listNames:string[] = ["Service","Yatch","Designs","tests","test2"]
+  careerNames:string[] = ["Sailor","Chef", "Boat Engineer"]
+  listData:any[] = [];
+  careerData:any[] = [];
+  constructor() { 
+    for(let i=0; i< this.listNames.length;i++) {
+        this.listData.push({
+          'name':this.listNames[i],
+          'style':"calc(32% - "+ (i==0 ? "4" : "-"+(4 * 5 + (i-1) * 24 )) +"px)"
+        });
+    }
+    for(let i=0; i< this.careerNames.length;i++) {
+      this.careerData.push({
+        'name':this.careerNames[i],
+        'style':"calc(32% - "+ (i==0 ? "4" : "-"+(4 * 5 + (i-1) * 24 )) +"px)"
+      });
+    }
+  }
 
   ngOnInit() {
   }

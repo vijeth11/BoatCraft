9ea5b591a9410b680377ae6d80ecda804afbbdba
vijeth11 <vijeth11@yahoo.co.in>
2021-03-20 13:07:12 -0400

    added career page

--- frontend/BoatCraft/src/app/career/career.component.ts
+++ frontend/BoatCraft/src/app/career/career.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-career',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CareerComponent implements OnInit {
 
-  constructor() { }
+  careerForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    careerselected: new FormControl(''),
+    about: new FormControl('')
+  });
+
+  public careers: string[] = ["Helpers","Machanics","Electicians","Spray Painters","Interior Designers"];
+  constructor() { 
+    window.scrollTo(0,0);
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+    console.log(this.careerForm);
+  }
 }

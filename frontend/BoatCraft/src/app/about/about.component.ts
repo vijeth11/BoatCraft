1d657ab1a8c1a6100eed29b1484e332dc410d6e9
vijeth11 <vijeth11@yahoo.co.in>
2021-03-22 13:20:40 -0400

    fixed mobile view

--- frontend/BoatCraft/src/app/about/about.component.ts
+++ frontend/BoatCraft/src/app/about/about.component.ts
@@ -13,11 +13,16 @@ export class AboutComponent implements OnInit {
   public title:string = 'About Us';
   public pathMap:string[] = ['Home', this.title];
   public aboutPara:string[] = [];
-
+  public imagesWidth:string = '11rem';
+  public imagesHeight:string = '11rem';
   constructor(private router: Router, private dataService:DataStoreService) {
     window.scrollTo(0, 0);
+    console.log(document.documentElement.clientWidth);
+    if(document.documentElement.clientWidth <= 500){
+      this.imagesWidth = "100%";
+      this.imagesHeight = "28rem";
+    }
     this.gallery = this.dataService.getPhotosForAbout();
-    console.log(this.gallery);
     this.aboutPara = this.dataService.getAbout();
    }
 

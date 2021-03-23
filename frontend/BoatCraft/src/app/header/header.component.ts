ca66573502d819a54a1cf7ffbb2e4b5863092366
vijeth11 <vijeth11@yahoo.co.in>
2021-03-23 03:56:26 -0400

    fixed mobile view

--- frontend/BoatCraft/src/app/header/header.component.ts
+++ frontend/BoatCraft/src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  navigate(data:string){
+    if (data == "home")
+      this.router.navigate(['']);
+    else
+      this.router.navigate([data]);
+  }
 }

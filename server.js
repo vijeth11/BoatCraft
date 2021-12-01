7efcba5522e7f50465f364cd9b968db4179273ed
vijeth11 <vijeth11@yahoo.co.in>
2021-12-01 10:41:41 -0500

    updated

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-    //Install express server
-    const express = require('express');
-    const path = require('path');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
-    const app = express();
+const express = require('express');
+const app = express();
 
-    // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/BoatCraft'));
+app.use(requireHTTPS);
+app.use(express.static('./dist/BoatCraft'));
 
-    app.get('/*', function(req,res) {
-        
-    res.sendFile(path.join(__dirname+'/dist/BoatCraft/index.html'));
-    });
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/BoatCraft/'}),
+);
 
-    // Start the app by listening on the default Heroku port
-    app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
